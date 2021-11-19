87a77444bff211343852405c1936b461174f89ef
Shung <wongshu2@msu.edu>
2021-11-18 23:39:30 -0500

    javascript soundboard

---
+++ soundboard.js
@@ -0,0 +1,28 @@
+function applauseFunc()
+{
+    new Audio(src="sounds/applause.wav").play();
+}
+
+function booFunc()
+{
+    new Audio(src="sounds/boo.wav").play();
+}
+
+function coughFunc()
+{
+    new Audio(src="sounds/cough.wav").play();
+}
+
+function applauseMouse()
+{
+    new Audio(src="sounds/applause.wav").play();
+}
+function booOver()
+{
+    new Audio(src="sounds/boo.wav").play();
+}
+
+function coughOver()
+{
+    new Audio(src="sounds/cough.wav").play();
+}
